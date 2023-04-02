8668697d0b9837f743781998afbc5e2c497ee1dc
Pobar19 <Abdoulaye Samb>
2023-04-02 20:02:51 +0000

    change mongd to mongodb

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/tddDabase")
+mongoose.connect("mongod://localhost:27017/tddDabase")
 .then(() => console.log("Connected to MongoDB"))
 .catch((err) => console.log("Failed to connect to MongoDB", err));
 
