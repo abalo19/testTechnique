c990525efdbe64216276708fa18eca9594441021
Pobar19 <Abdoulaye Samb>
2023-04-02 23:48:15 +0000

    some change

--- backend/dist/config/db.js
+++ backend/dist/config/db.js
@@ -4,6 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const mongoose_1 = __importDefault(require("mongoose"));
-mongoose_1.default.connect("mongodb://localhost:27017/tddDabase").then(() => console.log("Connected to MongoDB"))
+mongoose_1.default.connect("mongodb://localhost/tddDabase").then(() => console.log("Connected to MongoDB"))
     .catch((err) => console.log("Failed to connect to MongoDB", err));
 exports.default = mongoose_1.default.connection;
