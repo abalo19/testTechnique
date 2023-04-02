c08d541e793ac814f12f5f7b6ed9015824665524
Pobar19 <Abdoulaye Samb>
2023-04-02 18:43:48 +0000

    new dploy

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost/tddDabase")
+mongoose.connect("mongodb://localhost:27017/tddDabase")
 .then(() => console.log("Connected to MongoDB"))
 .catch((err) => console.log("Failed to connect to MongoDB", err));
 
