616ab437722cc236d7ca9d2bb0dfa4f5584984c7
Pobar19 <Abdoulaye Samb>
2023-04-03 10:08:48 +0000

    some change

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const port = process.env.PORT || 3000;
-app_1.app.listen(port, () => {
-    console.log(`Server started on port ${port}`);
-});
+// const port = process.env.PORT || 3000;
+// app_1.app.listen(port, () => {
+//     console.log(`Server started on port ${port}`);
+// });
